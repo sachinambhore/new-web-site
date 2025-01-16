198b28d5389ddb8729dd59faaaebb649c6235734
sachinambhore <sachin.ambhore.fs@gmail.com>
2025-01-17 00:59:43 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,9 +5,9 @@ echo "Stopping the web server..."
 sudo systemctl stop nginx || echo "Nginx is not running."
 
 # Optional: Check the process is stopped
-if pgrep nginx > /dev/null; then
-  echo "Failed to stop Nginx."
-  exit 1
-else
-  echo "Nginx stopped successfully."
-fi
+#if pgrep nginx > /dev/null; then
+  #echo "Failed to stop Nginx."
+  #exit 1
+#else
+ # echo "Nginx stopped successfully."
+#fi
